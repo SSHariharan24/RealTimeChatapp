597d9e8b2c1829f2db73319d8ba33e86bc699e08
SSHariharan24 <hariabi2004a@gmail.com>
2025-07-11 18:19:50 +0530

    recent newly commit

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,9 @@
-import axios from "axios"
+import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://realtimechatappserver-gge5.onrender.com/api",
-    withCredentials:true,
-})
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5001/api"
+      : "https://realtimechatappserver-gge5.onrender.com/api",
+  withCredentials: true,
+});
